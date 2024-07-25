89a04075c4f18d98df83b53fe455be323caef866
yyudaii <yudai200009089311@gmail.com>
2024-07-25 21:25:20 +0900

    バリデーションの追記

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -1,7 +1,7 @@
 class Lyric < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
+  validates :title, presence: true
   validates :lyric, presence: true
 
   scope :older_than_24_hours, -> { where('created_at < ?', 24.hours.ago) }
